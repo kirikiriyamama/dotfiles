67176f1613e4a826fa25382ead4f203fce5a47c5
remain <ponde.ponde.pond@gmail.com>
2013-05-22 14:01:08 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-find . -regex "^\.\/\.\w+$" |
+find . -regex "^\.\/\.[a-z.]+$" |
 sed -e "s/.\///" |
 while read dotfile
 do
-  case $dotfile in
+  case ${dotfile} in
     .git) continue;;
   esac
 
