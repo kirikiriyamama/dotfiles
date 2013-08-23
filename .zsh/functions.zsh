1df07c5dff1f3e4a62dad73498e9104222930939
remain <ponde.ponde.ponde@gmail.com>
2013-08-23 18:05:17 +0900

    ls and git-status are displayed by Enter
    add ls_abbrev
    fix cdup

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,8 +1,45 @@
-function chpwd() { ls -F --color=auto }
+function ls_abbrev() {
+  if [[ ! -r $PWD ]]; then
+    return
+  fi
+
+  local ls_result
+  ls_result=$(COLUMNS=$COLUMNS ls -CF --color=always | sed $'/^\e\[[0-9;]*m$/d')
+
+  if [ $(echo $ls_result | wc -l) -gt 12 ]; then
+    echo $ls_result | head -n 5
+    echo '...'
+    echo $ls_result | tail -n 5
+    echo "$(ls -1 | wc -l) files exist"
+  else
+    echo $ls_result
+  fi
+}
+
+function dir_status() {
+  if [ -n "$BUFFER" ]; then
+    zle accept-line
+    return 0
+  fi
+  echo
+  ls_abbrev
+  if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
+    echo
+    echo "--- git status ---"
+    git status -sb
+  fi
+  echo
+  zle reset-prompt
+}
+zle -N dir_status
+bindkey '^m' dir_status
+
+function chpwd() { ls_abbrev }
 
 function cdup() {
   echo
   cd ..
+  echo
   zle reset-prompt
 }
 zle -N cdup
