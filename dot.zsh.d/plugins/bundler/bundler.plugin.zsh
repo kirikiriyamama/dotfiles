d3ef1fecd57ac63f893e15ad03e5aa831a7631cb
remain <ponde.ponde.ponde@gmail.com>
2013-10-11 22:02:05 +0900

    add zsh plugin bundler

---
+++ dot.zsh.d/plugins/bundler/bundler.plugin.zsh
@@ -0,0 +1,37 @@
+# The following is based on https://github.com/gma/bundler-exec
+
+bundled_commands=(atig berks cap foodcritic foreman guard jekyll knife middleman pry rackup rainbows rails rake rspec ruby shotgun spec spork strainer thin thor unicorn unicorn_rails puma)
+
+## Functions
+
+_bundler-installed() {
+  which bundle > /dev/null 2>&1
+}
+
+_within-bundled-project() {
+  local check_dir=$PWD
+  while [ $check_dir != "/" ]; do
+    [ -f "$check_dir/Gemfile" ] && return
+    check_dir="$(dirname $check_dir)"
+  done
+  false
+}
+
+_run-with-bundler() {
+  if _bundler-installed && _within-bundled-project; then
+    bundle exec $@
+  else
+    $@
+  fi
+}
+
+## Main program
+for cmd in $bundled_commands; do
+  eval "function unbundled_$cmd () { $cmd \$@ }"
+  eval "function bundled_$cmd () { _run-with-bundler $cmd \$@}"
+  alias $cmd=bundled_$cmd
+
+  if which _$cmd > /dev/null 2>&1; then
+        compdef _$cmd bundled_$cmd=$cmd
+  fi
+done
