87b51222d72712f1124609a05edeb3a58a03dd9a
remain <ponde.ponde.ponde@gmail.com>
2013-10-28 11:19:23 +0900

    take measures to 'zsh: no matches found'

--- dot.zsh.d/completion.zsh
+++ dot.zsh.d/completion.zsh
@@ -5,5 +5,7 @@ setopt magic_equal_subst
 setopt auto_pushd
 setopt pushd_ignore_dups
 
+setopt nonomatch
+
 zstyle ':completion:*:sudo:*' command-path $PATH
 zstyle ':completion:*:*:*:*:processes' command "ps x -o pid,user,args"
