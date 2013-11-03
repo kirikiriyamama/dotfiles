b9f2b57f3ad6479aaa57cc15b94bec2f8fb9bc69
remain <ponde.ponde.ponde@gmail.com>
2013-11-03 15:08:44 +0900

    fix ruby_version

--- dot.tmux.d/segments/ruby_version.sh
+++ dot.tmux.d/segments/ruby_version.sh
@@ -1,6 +1,6 @@
 run_segment() {
   local env_name=$(tmux display -p "TMUXRUBY_#D" | tr -d %)
-  local env_val=$(tmux show-environment | grep --color=never "$env_name")
+  local env_val=$(tmux show-environment | grep --color=never "$env_name=")
 
   if [[ ! $env_val =~ "unknown variable" ]]; then
     local tmux_ruby=$(echo "$env_val" | sed 's/^.*=//')
