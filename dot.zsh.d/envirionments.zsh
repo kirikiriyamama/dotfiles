1dccc9bf280a62b1909697fa7fbaabbb43c3cdcf
remain <ponde.ponde.ponde@gmail.com>
2014-01-14 15:00:24 +0900

    fix duplicate path on subshell

--- dot.zsh.d/envirionments.zsh
+++ dot.zsh.d/envirionments.zsh
@@ -1,4 +1,11 @@
-export PATH=$PATH:$HOME/dev/bin
+function merge_path() {
+  case ":${PATH}:" in
+    *:"$1":*) ;;
+    *) export PATH=$PATH:$1 ;;
+  esac
+}
+
+merge_path $HOME/dev/bin
 
 export EDITOR=vim
 export LANG=en_US.UTF-8
