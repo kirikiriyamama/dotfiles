05623af19ee9290cd9e5a5f3fb01417cb7f72457
kirikiriyamama <ponde.ponde.ponde@gmail.com>
2014-11-19 23:50:31 +0900

    Modify aliases

--- dot.zsh.d/aliases.zsh
+++ dot.zsh.d/aliases.zsh
@@ -2,11 +2,12 @@ alias ls='ls -F --color=auto'
 alias ll='ls -l'
 alias la='ls -lA'
 
-alias sudo='sudo -E '
+alias less='less -r'
 alias sqlite='sqlite3'
+alias sudo='sudo -E '
+alias tmux='tmux -2 -u'
 alias vi='vim'
 alias yum='yum -y'
-alias tmux='tmux -2 -u'
 
 if type colordiff &> /dev/null; then
   alias diff='colordiff -u'
@@ -22,15 +23,9 @@ alias grt='cd $(git rev-parse --show-toplevel 2> /dev/null || echo ".")'
 
 alias tailf='tail -f'
 
-alias -g :x='| xargs'
-alias -g :xg='| xargs grep'
-alias -g :g='| grep'
-alias -g :gv='| grep -v'
 alias -g :l='| less'
-alias -g :lr='| less -r'
 alias -g :h='| head'
 alias -g :t='| tail'
-alias -g :tf='| tail -f'
 alias -g :p='| peco'
 
 alias @d='~/dev'
