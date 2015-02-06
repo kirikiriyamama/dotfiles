67efc89ea6ce031e10ef83b40e7c8d52fee97c33
kirikiriyamama <kirikiriyamama@icloud.com>
2015-02-06 16:24:59 +0900

    Cosmetic change :lipstick:

--- dot.vim/ftdetect/filetype.vim
+++ dot.vim/ftdetect/filetype.vim
@@ -1,8 +1,8 @@
 augroup filetypedetect
-  autocmd BufNewFile,BufRead *.thor setfiletype ruby
-  autocmd BufNewFile,BufRead Guardfile setfiletype ruby
   autocmd BufNewFile,BufRead Capfile setfiletype ruby
+  autocmd BufNewFile,BufRead Guardfile setfiletype ruby
   autocmd BufNewFile,BufRead *.coffee setfiletype coffee
+  autocmd BufNewFile,BufRead *.thor setfiletype ruby
   autocmd BufNewFile,BufRead *.template setfiletype json
   autocmd BufNewFile,BufRead *.zshrc setfiletype zsh
   autocmd BufNewFile,BufRead *.zsh-theme setfiletype zsh
--- dot.zsh.d/functions/dir_status.zsh
+++ dot.zsh.d/functions/dir_status.zsh
@@ -1,21 +1,21 @@
 function git_status() {
-  if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) = 'true' ]]; then
     echo
     echo "--- git status ---"
     git status -sb
 
     # stash list
     stash=$(git stash list)
-    if [ -n "$stash" ]; then
+    if [[ -n ${stash} ]]; then
       echo
       echo "--- git stash ---"
-      echo $stash
+      echo ${stash}
     fi
   fi
 }
 
 function dir_status() {
-  if [ -n "$BUFFER" ]; then
+  if [[ -n ${BUFFER} ]]; then
     zle accept-line
     return 0
   fi
