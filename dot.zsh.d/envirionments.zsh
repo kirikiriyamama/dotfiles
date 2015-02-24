11cb29f3354f4879155926455602b8784687ab00
kirikiriyamama <kirikiriyamama@icloud.com>
2015-02-24 16:28:18 +0900

    Cosmetic change

--- dot.zsh.d/envirionments.zsh
+++ dot.zsh.d/envirionments.zsh
@@ -1,5 +1,5 @@
 export LANG='en_US.UTF-8'
-export LC_CTYPE=$LANG
+export LC_CTYPE=${LANG}
 
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;32'
@@ -8,8 +8,8 @@ export EDITOR='vim'
 export PAGER='less'
 
 # for powerline
-export XDG_CONFIG_DIRS=$HOME/.powerline.d
+export XDG_CONFIG_DIRS=${HOME}/.powerline.d
 
-export GOPATH=$HOME/.go
+export GOPATH=${HOME}/.go
 export PATH=${HOME}/dev/bin:${PATH}
 export PATH=${GOPATH}/bin:${PATH}
