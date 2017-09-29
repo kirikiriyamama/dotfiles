448271d63db13d8801a8c3cceae3cc583ef0067d
kirikiriyamama <kirikiriyamama@gmail.com>
2017-09-29 16:06:54 +0900

    Refactor install.sh

--- README.md
+++ README.md
@@ -5,24 +5,11 @@
 $ git clone https://github.com/kirikiriyamama/dotfiles.git
 $ cd dotfiles && ./install.sh
 
-$ vim +PlugInstall +qa
-
 $ sudo echo '/usr/local/bin/zsh' >>/etc/shells
 $ chsh -s /usr/loca/bin/zsh
 ```
 
 ## Requirements
- * Zsh
- * Git
- * tmux
- * Vim
- * GNU sed
-  * See also [GNU sed](http://www.gnu.org/software/sed/)
-  * For OSX
- * GNU core utilities
-  * See also [GNU core utilities](http://www.gnu.org/software/coreutils/)
-  * For OSX
-<pre><code>$ brew install coreutils
-$ vim ~/.zshrc.local
-+PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
-</code></pre>
+
+- Git
+- [Homebrew](https://brew.sh/)
--- install.sh
+++ install.sh
@@ -1,38 +1,11 @@
 #!/bin/bash
+set -o errexit -o pipefail -o xtrace
 
-# initialize
 repository=$(cd $(dirname ${0}); pwd)
+cd ${repository}
 
-case ${OSTYPE} in
-  darwin*) PLATFORM="osx" ;;
-  *) PLATFORM="unknown" ;;
-esac
-
-find_opts=()
-[ "${PLATFORM}" = "osx" ] && find_opts=("-E")
-
-# update submodules
-(cd ${repository} && git submodule update --init)
-
-# make directories
-dirs=("dev")
-for dir in ${dirs[@]}; do
-  target="${HOME}/${dir}"
-  if [ ! -e "${target}" ]; then
-    command="mkdir ${target}"
-    echo ${command} && command ${command}
-  fi
-done
-
-# create symlinks
-files=($(find ${find_opts[@]} ${repository} -regex "${repository}/dot\.[^/]+$" | sed -e "s|${repository}/||") "dev/bin")
-for file in ${files[@]}; do
-  source="${repository}/${file}"
-  target="${HOME}/$(echo ${file} | sed -e "s|^dot||g")"
-  if [ ! -e "${target}" ]; then
-    command="ln -s ${source} ${target}"
-    echo ${command} && command ${command}
-  else
-    echo "${target} exists"
-  fi
-done
+git submodule update --init
+grep -v -e '^$' -e '^#' Brewfile | xargs -t -L 1 brew
+find home -mindepth 1 -maxdepth 1 | sed -e 's|home/||' | xargs -t -I {} ln -s ${repository}/{} ${HOME}
+vim +PlugInstall +qa
+${HOME}/.tmux/plugins/tpm/bin/install_plugins
