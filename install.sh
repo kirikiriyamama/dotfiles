f60510ba13c7990074b29bb0a840e2d0de789d38
kirikiriyamama <kirikiriyamama@gmail.com>
2017-10-12 15:49:02 +0900

    Make sure to exit if a command fails

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ repository=$(cd $(dirname ${0}); pwd)
 cd ${repository}
 
 git submodule update --init
-grep -v -e '^$' -e '^#' Brewfile | xargs -t -L 1 brew
-find home -mindepth 1 -maxdepth 1 | sed -e 's|home/||' | xargs -t -I {} ln -s ${repository}/{} ${HOME}
+while read c; do
+  brew ${c}
+done < <(grep -v -e '^$' -e '^#' Brewfile)
+while read f; do
+  ln -s ${repository}/${f} ${HOME}
+done < <(find home -mindepth 1 -maxdepth 1)
 vim +PlugInstall +qa
 ${HOME}/.tmux/plugins/tpm/bin/install_plugins
