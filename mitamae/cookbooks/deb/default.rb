586f889685148b09f74b56d2b2ece8ebc407466f
kirikiriyamama <kirikiriyamama@gmail.com>
2020-12-15 19:42:00 +0900

    Make packages upgradable

--- mitamae/cookbooks/deb/default.rb
+++ mitamae/cookbooks/deb/default.rb
@@ -1,16 +1,24 @@
-define :deb, url: nil do
+define :deb, url: nil, version: nil do
   workdir = '/tmp'
 
   name = params[:name]
   url = params[:url]
+  version = params[:version]
+
+  condition =
+    if version.nil?
+      "dpkg -l #{name}"
+    else
+      %Q{test "$(dpkg-query -W -f '${Version}' #{name})" = "#{version}"}
+    end
 
   execute 'fetch' do
     command "curl -fsSLo #{workdir}/#{name} #{url}"
-    not_if "dpkg -l #{name}"
+    not_if condition
   end
 
   execute 'install' do
     command "dpkg -i #{workdir}/#{name}"
-    not_if "dpkg -l #{name}"
+    not_if condition
   end
 end
