81d5ec2b7b5b3f70a4caeccc07af947ba09b7576
kirikiriyamama <kirikiriyamama@gmail.com>
2020-12-15 19:42:57 +0900

    Upgrade gh

--- mitamae/cookbooks/gh/default.rb
+++ mitamae/cookbooks/gh/default.rb
@@ -1,6 +1,7 @@
-version = '1.1.0'
+ver = '1.3.1'
 arch = 'amd64'
 
 deb 'gh' do
-  url "https://github.com/cli/cli/releases/download/v#{version}/gh_#{version}_linux_#{arch}.deb"
+  url "https://github.com/cli/cli/releases/download/v#{ver}/gh_#{ver}_linux_#{arch}.deb"
+  version ver
 end
