803264b32b0efe018c1feac5612b0377eee7180c
kirikiriyamama <kirikiriyamama@gmail.com>
2021-11-14 16:32:54 +0900

    Add missing symlink

--- mitamae/cookbooks/zsh/default.rb
+++ mitamae/cookbooks/zsh/default.rb
@@ -4,6 +4,7 @@ execute "chsh #{node[:user]} -s /usr/bin/zsh" do
   not_if "test \"$(getent passwd #{node[:user]} | cut -d: -f7)\" = /usr/bin/zsh"
 end
 
+dotfile '.dircolors'
 dotfile '.zshrc'
 dotfile '.zshenv'
 dotfile '.zsh.d'
