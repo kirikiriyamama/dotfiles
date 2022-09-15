cc0801fb6aadaecbfcf41e1be7eb5a337d48df5a
kirikiriyamama <kirikiriyamama@gmail.com>
2022-09-15 12:52:02 +0900

    NERDTree だけになったタブも閉じる

--- home/.vim/lua/plugins.lua
+++ home/.vim/lua/plugins.lua
@@ -27,8 +27,8 @@ return require('packer').startup(function(use)
         endfunction
         nnoremap <silent> <Space>e :call <SID>toggle()<CR>
 
-        " Exit Vim if NERDTree is the only window left.
-        autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTreeType") && b:NERDTreeType == "primary") | q | endif
+        " Close the tab if NERDTree is the only window remaining in it.
+        autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
       ]]
 
       vim.g.NERDTreeQuitOnOpen = true
