862e88c99b60d671b037baedb953f1157d2b1795
kirikiriyamama <kirikiriyamama@gmail.com>
2022-12-20 16:45:50 +0900

    self-upgrade するから clone だけしてほしい

--- mitamae/cookbooks/asdf/default.rb
+++ mitamae/cookbooks/asdf/default.rb
@@ -1,6 +1,9 @@
-git "#{node[:home]}/.asdf" do
+asdf = "#{node[:home]}/.asdf"
+
+git asdf do
   repository 'https://github.com/asdf-vm/asdf.git'
   user node[:user]
+  not_if "test -d #{asdf}"
 end
 
 dotfile '.asdfrc'
--- mitamae/cookbooks/nvim/default.rb
+++ mitamae/cookbooks/nvim/default.rb
@@ -1,10 +1,14 @@
 package 'neovim'
+
 include_cookbook 'ctags'
 
 dotfile '.config/nvim'
 
-git "#{node[:home]}/.local/share/nvim/site/pack/packer/start/packer.nvim" do
+packer = "#{node[:home]}/.local/share/nvim/site/pack/packer/start/packer.nvim"
+
+git packer do
   repository 'https://github.com/wbthomason/packer.nvim'
   depth 1
   user node[:user]
+  not_if "test -d #{packer}"
 end
