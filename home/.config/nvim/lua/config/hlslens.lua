f5f1253ddfd7577cb81558132ed6e24993562d12
kirikiriyamama <kirikiriyamama@gmail.com>
2023-03-29 19:29:24 +0900

    いらなそうだった

--- home/.config/nvim/lua/config/hlslens.lua
+++ home/.config/nvim/lua/config/hlslens.lua
@@ -1,9 +1,7 @@
 require('hlslens').setup({ nearest_only = true })
 
-local kopts = { silent = true }
-
-vim.keymap.set('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>zz]], kopts)
-vim.keymap.set('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>zz]], kopts)
+vim.keymap.set('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>zz]])
+vim.keymap.set('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>zz]])
 vim.keymap.set('n', '*', function()
   require('lasterisk').search()
   require('hlslens').start()
@@ -11,4 +9,4 @@ end)
 vim.keymap.set({ 'n', 'x' }, 'g*', function()
   require('lasterisk').search({ is_whole = false })
   require('hlslens').start()
-end, kopts)
+end)
