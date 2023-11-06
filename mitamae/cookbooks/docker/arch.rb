26ee3e7bf963048df21cd83538b3298f00fffe80
kirikiriyamama <kirikiriyamama@gmail.com>
2023-11-06 12:38:27 +0900

    MySQL メモリ食べすぎ

--- mitamae/cookbooks/docker/arch.rb
+++ mitamae/cookbooks/docker/arch.rb
@@ -5,6 +5,12 @@ service 'docker.service' do
   action [:enable, :start]
 end
 
+# https://zenn.dev/23prime/articles/a54d986abdc0d8
+remote_file '/etc/docker/daemon.json' do
+  mode '600'
+  notifies :restart, 'service[docker.service]'
+end
+
 execute "usermod -aG docker #{node[:user]}" do
   not_if "getent group docker | grep #{node[:user]}"
 end
