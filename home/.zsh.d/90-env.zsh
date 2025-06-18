514618ff1b4d2c7e8b3115432ae9e6862763c716
kirikiriyamama <kirikiriyamama@gmail.com>
2025-06-18 12:46:45 +0900

    名前つけておく

--- home/.zsh.d/90-env.zsh
+++ home/.zsh.d/90-env.zsh
@@ -1,5 +1,3 @@
 export PATH="${HOME}/.local/bin:${PATH}"
 export PATH="${HOME}/bin:${PATH}"
 export PATH="${HOME}/bin.local:${PATH}"
-
-test -f /etc/profile.d/google-cloud-cli.sh && source /etc/profile.d/google-cloud-cli.sh
---
+++ home/.zsh.d/90-gcloud.zsh
@@ -0,0 +1,1 @@
+test -f /etc/profile.d/google-cloud-cli.sh && source /etc/profile.d/google-cloud-cli.sh
