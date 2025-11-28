6c5aac268296de71d2fb1cfe0ac7b75802d068a6
kirikiriyamama <kirikiriyamama@gmail.com>
2025-11-28 13:31:55 +0900

    補完されなくなってた
    前からだっけ？

--- home/.zsh.d/90-gcloud.zsh
+++ home/.zsh.d/90-gcloud.zsh
@@ -1,1 +1,2 @@
 test -f /etc/profile.d/google-cloud-cli.sh && source /etc/profile.d/google-cloud-cli.sh
+test -f /opt/google-cloud-cli/completion.zsh.inc && source /opt/google-cloud-cli/completion.zsh.inc
