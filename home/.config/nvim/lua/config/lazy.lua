01e506c9dce1398efb7a7cd407c0d46dd0832cfb
kirikiriyamama <kirikiriyamama@gmail.com>
2026-02-25 12:06:19 +0900

    いまどこにいるかわかりやすくする
    ref: https://x.com/commte/status/2025781190230495733

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -59,6 +59,7 @@ require("lazy").setup({
         vim.opt.background = 'dark'
         vim.opt.termguicolors = true
         vim.cmd('colorscheme gruvbox')
+        vim.cmd('highlight Normal guibg=None')
       end,
     },
     {
